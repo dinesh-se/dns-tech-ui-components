e80a20f47ccb74af7497c094aa352195679f88b5
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2021-01-31 13:06:16 +0000

    support assets

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -6,15 +6,32 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.css$/,
+        test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        }
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets',
+            publicPath: 'assets',
+          }
+        }]
       }
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
-      title: 'Development',
     }),
   ]
 }

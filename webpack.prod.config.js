a8e5f9156f9f1d1dac634dd48b9351e2b6f10e22
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2021-01-31 09:49:04 +0000

    Added clean-webpack-plugin

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const commonConfig = require('./webpack.common.config');
 
@@ -9,4 +10,5 @@ module.exports = merge(commonConfig, {
     filename: 'main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new CleanWebpackPlugin()],
 });

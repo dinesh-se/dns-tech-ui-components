a9e0594a0a373b33f1a712f5df8296ea9910e2e5
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2021-02-12 07:24:13 +0000

    Package upgrades and clean-up

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import cn from 'classnames';
 
 import { add } from './utils/addition';
-import '../styles/main.scss';
+
+const StyledArticle = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+`;
 
 const App = () => (
-  <article className="flex-container">
+  <StyledArticle>
     <p>Addition (10 + 5): <span className="result-node">{add(5, 10)}</span></p>
     <img src="../assets/dinesh-haribabu.jpeg" />
-    <button className="btn btn-success">Button</button>
-  </article>
+    <button>Button</button>
+  </StyledArticle>
 );
 
 export default App;

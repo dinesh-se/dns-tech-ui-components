3a317fa955e3d053d5a29a23f4052dc51dfaf788
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2021-03-10 21:08:08 +0000

    test: fix for storyshots

--- tests/storyshots.js
+++ tests/storyshots.js
@@ -7,10 +7,24 @@ const getMatchOptions = () => ({
   failureThresholdType: 'percent',
 })
 
+const beforeScreenshot = (page) => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      page.setViewport({
+        width: 800,
+        height: 600,
+        deviceScaleFactor: 1,
+      })
+      resolve()
+    })
+  )
+}
+
 initStoryshots({
   suite: 'Image storyshots',
   test: imageSnapshot({
     storybookUrl: `file:///${path.join(__dirname, '..', 'storybook-static')}`,
+    beforeScreenshot,
     getMatchOptions,
   }),
 })
